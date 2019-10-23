6b1e43fb6921a4831c4ccd6ec8b687fe4b66a882
jaczh <jjsmailmail@gmail.com>
2019-10-22 18:20:56 -0700

    commit
    clean code

--- src/main/java/selenium_mvn_java/selenium_mvn_java_1/App.java
+++ src/main/java/selenium_mvn_java/selenium_mvn_java_1/App.java
@@ -1,6 +1,5 @@
 package selenium_mvn_java.selenium_mvn_java_1;
 
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,7 +15,7 @@ public class App
 {
 	public static WebDriver driver = null;
 	
-	public static WebDriver openBrowser( String baseUrl) {
+	public static WebDriver openBrowser( String baseUrl ) {
 		System.setProperty("webdriver.gecko.driver", "/Users/JacZh/Desktop/resources/selenium_java/webdrivers/geckodriver");
 		driver = new FirefoxDriver();
 		driver.get(baseUrl);
@@ -28,9 +27,16 @@ public class App
 		return map;
 	}
 	
+	public static List<String> getPairList( String filePath ) throws IOException {
+		List<String> pairList = GetInfo.getPairList(filePath);
+		return pairList;
+	}
+	
+	public static void goToPage (String page_name_xpath) {
+		clickBtn(page_name_xpath);
+	}
+	
 	public static List<WebElement> locateElement ( List<String> elements) {
-
-//		System.out.println(driver.getPageSource());
 
 		List<WebElement> elementFields = new ArrayList<>();
 		for (int i = 0; i<elements.size(); i++) {
@@ -41,42 +47,41 @@ public class App
 		return elementFields;
 	}
 
-	public static void sendKeys( List<WebElement> fields, List<String> txt) {
-		for (int i = 0;i<=fields.size();i++){
-			fields.get(i).sendKeys(txt.get(i));
+	public static void sendKeys( List<WebElement> fields, List<String> txtToSend) {
+		for (int i = 0;i<fields.size();i++){
+			fields.get(i).sendKeys(txtToSend.get(i));
 		}
 	}
 
+	public static void clickBtn( String btn_xpath) {
+		List<String> btn_xpath_list = new ArrayList<>();
+		btn_xpath_list.add(btn_xpath);
+		List<WebElement> btn_element = locateElement(btn_xpath_list);
+		btn_element.get(0).click();
+	}
 	
-	public static void logIn() throws IOException {
-//		Map<String, String> account = readFile("src/main/java/files/account_info");
-//		String[] account = ["admin","12345"];
-		List<String> account = Arrays.asList("admin", "12345");
-		Map<String, String> map = readFile("src/main/java/files/web_info");
-
-		//click log in
-		List<String> x = new ArrayList<>();
-		String loginPage = map.get("login_page");
-		x.add(loginPage);
-//		List<String> xpaths = new ArrayList<>(map.values());
-		List<WebElement> elementPath = locateElement(x);
-		elementPath.get(0).click();
+	public static void logIn(List<String> account_info_list,String user_path, String pwd_path, String login_btn_path) throws IOException {		
 
 		//fill in log-in info
 		List<String> logInElement = new ArrayList<>();
-		String user_path = map.get("user_name");
 		logInElement.add(user_path);
-		String pwd_path = map.get("pwd");
 		logInElement.add(pwd_path);
 		List<WebElement> logIn_fields = locateElement(logInElement);
-
-		sendKeys(logIn_fields,account);
-
+		sendKeys(logIn_fields,account_info_list);
+		
+		//click log_in subimit btn
+		clickBtn(login_btn_path);
 	}
 	
-    public static void main( String[] args ) throws IOException {
-		String baseUrl="http://testing-ground.scraping.pro";
-		driver = openBrowser (baseUrl);
-		logIn();
+
+    public static void main( String[] args ) throws IOException {		
+//    	Map<String, String> account_map = readFile("src/main/java/files/account_info");
+    	List<String> account_info_list = getPairList("src/main/java/files/account_info");
+    	Map<String, String> map = readFile("src/main/java/files/web_info");
+		driver = openBrowser (map.get("baseUrl"));
+		
+		goToPage(map.get("login_page"));
+		logIn(account_info_list, map.get("user_name"), map.get("pwd"),map.get("login_btn"));
+		
     }
 }
--- src/main/java/selenium_mvn_java/selenium_mvn_java_1/GetInfo.java
+++ src/main/java/selenium_mvn_java/selenium_mvn_java_1/GetInfo.java
@@ -8,8 +8,6 @@ public class GetInfo {
     public static Map<String, String> generateMap(String filePath ) throws IOException {
 
         Map<String, String> map = new HashMap<String, String>();
-
-//        File file = new File("src/main/java/files/web_info");
         
         File file = new File(filePath);
 
@@ -19,7 +17,7 @@ public class GetInfo {
 
         while ((lines = br.readLine()) != null) {
             String[] element_xpath = lines.split("=", 2);
-            if (element_xpath.length >= 2) {
+            if (element_xpath.length > 1) {
                 String key = element_xpath[0];
                 String value = element_xpath[1];
                 map.put(key, value);
@@ -27,4 +25,46 @@ public class GetInfo {
         }
         return map;
     }
+    
+    public static List<String> getPairList (String account_filePath) throws IOException {
+    	List<String> account_info_list = new ArrayList<>();
+    	
+    	File file = new File(account_filePath);
+    	
+    	BufferedReader br = new BufferedReader(new FileReader(file));
+
+        String lines;
+        while ((lines = br.readLine()) != null) {
+        	String[] account_lines = lines.split("=", 2);
+            if (account_lines.length > 1) {
+            	String account_name = account_lines[0];
+            	String account_pwd = account_lines[1];
+            	account_info_list.add(account_name);
+            	account_info_list.add(account_pwd);
+            }
+        }    	
+    	return account_info_list;
+//    	return [id,pwd]    	
+    }
+    
+//    public static List<Map<String, String>> getAccount (String account_filePath) throws IOException {
+//    	Map<String, String> map = new HashMap<String, String>();
+//    	List<Map<String, String>> account_list = new ArrayList<>();
+//    	File file = new File(account_filePath);
+//    	
+//    	BufferedReader br = new BufferedReader(new FileReader(file));
+//
+//        String lines;
+//        while ((lines = br.readLine()) != null) {
+//            String[] element_xpath = lines.split("=", 2);
+//            if (element_xpath.length > 1) {
+//                String key = element_xpath[0];
+//                String value = element_xpath[1];
+//                map.put(key, value);
+//                account_list.add(map);
+//            } 
+//        }      
+//		return account_list;
+//		return [ { id : pwd } ]   	
+//    }
 } 
